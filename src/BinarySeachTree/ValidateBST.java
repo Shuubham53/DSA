9822752afbfedba13dfd0bee214575249cc6342b
Shuubham53 <shubhamnishad110@gmail.com>
2026-04-25 21:19:07 +0530

    Validate Binary Search Tree : optimal appproch

--- src/BinarySeachTree/ValidateBST.java
+++ src/BinarySeachTree/ValidateBST.java
@@ -13,21 +13,35 @@ public class ValidateBST {
             this.val = val;
         }
     }
+    public static boolean inorder(Node root,Node[] prev){
+        if(root == null) return true;
+        if(inorder(root.left,prev) == false) return false;
+        if(prev[0] != null && root.val <= prev[0].val){
+            return false;
+        }
+        prev[0] = root;
+        return inorder(root.right,prev);
 
-    public static void inorder(Node root, ArrayList<Integer> arr){
-        if(root == null) return;
-        inorder(root.left,arr);
-        arr.add(root.val);
-        inorder(root.right,arr);
     }
     public static boolean isValidBST(Node root) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        inorder(root,ans);
-        for(int i=1; i<ans.size();i++){
-            if(ans.get(i-1) >= ans.get(i)) return false;
-        }
-        return true;
+        Node[] prev = new Node[1];
+        return inorder(root,prev);
     }
+
+//    public static void inorder(Node root, ArrayList<Integer> arr){
+//        if(root == null) return;
+//        inorder(root.left,arr);
+//        arr.add(root.val);
+//        inorder(root.right,arr);
+//    }
+//    public static boolean isValidBST(Node root) {
+//        ArrayList<Integer> ans = new ArrayList<>();
+//        inorder(root,ans);
+//        for(int i=1; i<ans.size();i++){
+//            if(ans.get(i-1) >= ans.get(i)) return false;
+//        }
+//        return true;
+//    }
     public static void main(String[] args) {
         Node root = new Node(6);
         Node a = new Node(2);
