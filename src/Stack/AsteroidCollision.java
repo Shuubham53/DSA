c147ccdfdf370cd95342af528c6d28fe57b44e19
Shuubham53 <shubhamnishad110@gmail.com>
2026-04-26 18:48:06 +0530

    Asteroid Collision stack problem

--- src/Stack/AsteroidCollision.java
+++ src/Stack/AsteroidCollision.java
@@ -12,22 +12,11 @@ public class AsteroidCollision {
         int[] arr = {5,10,-5};
 
         Stack<Integer> st = new Stack<>();
-        ArrayList<Integer> ans = new ArrayList<>();
-
-        for(int ele: arr){
-
-            while(ele < 0 && !st.isEmpty() && st.peek() > 0) {
-                if(st.peek() < -ele) {
-                    st.pop(); // top explodes
-                } else if(st.peek() == -ele) {
-                    st.pop(); // both explode
-                    break;
-                } else {
-                    break;// incoming asteroid explodes
-                }
-            }
-            st.push(ele);
-
+        for(int a : arr){
+            while(!st.isEmpty() && st.peek() > 0 && a < 0 && st.peek() < -a) st.pop();
+            if(!st.isEmpty() && a < 0 && st.peek() > 0){
+                if(st.peek() == -a) st.pop();
+            }else st.push(a);
         }
         int n = st.size();
         int[] result = new int[n];
